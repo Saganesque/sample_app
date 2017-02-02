f47df740875131eb3a63bbd094f7d0622b037990
Saganesque <forvendorsonly@gmail.com>
2017-02-02 14:20:11 -0800

    added about.html.erb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
+=begin
+  StaticPagesController is a Ruby class, but because it inherits from ApplicationController the behavior of its methods is specific to Rails: when visiting the URL /static_pages/home, Rails looks in the Static Pages controller and executes the code in the home action, and then renders the view
+=end
+
+
+
   def home
   end
 
